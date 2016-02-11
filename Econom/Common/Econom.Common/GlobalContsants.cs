818224b500e2c73d062937bc328a0a7c4ed97422
MariyaSteffanova <mariya.steffanova@gmail.com>
2016-02-11 21:03:39 +0200

    Global Contsants file added

---
+++ Econom/Common/Econom.Common/GlobalContsants.cs
@@ -0,0 +1,11 @@
+﻿namespace Econom.Common
+{
+    public static class GlobalConstants
+    {
+        public const string DataServicesAssembly = " Econom.Services.Data";
+
+        public const int FirstNameMaxLength = 25;
+
+        public const int LastNameMaxLength = 25;
+    }
+}
