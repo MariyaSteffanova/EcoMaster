9ba33c5bd49ed63a4b06412e5dcdb20b919666b2
MariyaSteffanova <mariya.steffanova@gmail.com>
2016-02-11 21:12:09 +0200

    [DATA] [ItemMaster] [Models]

---
+++ Econom/Data/ItemMasterData/ItemMasterData.Models/Image.cs
@@ -0,0 +1,19 @@
+﻿namespace ItemMasterData.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Image
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string MimeType { get; set; }
+
+        public string Url { get; set; }
+    
+        public int? ProductId { get; set; }
+
+       // public virtual Product Product { get; set; }
+    }
+}
---
+++ Econom/Data/ItemMasterData/ItemMasterData.Models/Product.cs
@@ -0,0 +1,40 @@
+﻿namespace ItemMasterData.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Product
+    {
+        private ICollection<Image> images;
+
+        public Product()
+        {
+            this.images = new HashSet<Image>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        public string Barcode { get; set; }
+
+        public string Description { get; set; }
+
+        public string Manufacturer { get; set; }
+
+        public int UnitPerPackage { get; set; }
+
+        public string Ingredients { get; set; }
+
+        public int? CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+
+        public virtual ICollection<Image> Images
+        {
+            get { return this.images; }
+            set { this.images = value; }
+        }
+    }
+}
