6c8bad5c85bc3f37939df40fd7cb0176c1aad0fa
MariyaSteffanova <mariya.steffanova@gmail.com>
2016-02-11 21:09:59 +0200

    [Services][Recognizers] init

---
+++ Econom/Services/Econom.Services.Recognizers/Contracts/IBarcodeRecognizerService.cs
@@ -0,0 +1,12 @@
+﻿namespace Econom.Services.Recognizers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IBarcodeRecognizerService
+    {
+    }
+}
