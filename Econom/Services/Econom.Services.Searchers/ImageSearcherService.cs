18f75ee48e80b0f76c6fbf03ba51ef178d2805de
MariyaSteffanova <mariya.steffanova@gmail.com>
2016-02-12 00:11:31 +0200

    [Services][Searchers]

---
+++ Econom/Services/Econom.Services.Searchers/ImageSearcherService.cs
@@ -0,0 +1,15 @@
+﻿namespace Econom.Services.Searchers
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Econom.Services.Searchers.Contracts;
+
+    public class ImageSearcherService : IImageSearcherService
+    {
+        public ICollection<string> FindImages(string searchTerm)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
