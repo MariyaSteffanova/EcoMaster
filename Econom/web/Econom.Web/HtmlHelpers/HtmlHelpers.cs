25a0d2091637b86dfcb0d1359e04080088d85044
MariyaSteffanova <mariya.steffanova@gmail.com>
2016-02-14 10:13:24 +0200

    [Web] Html Helpers for images added

---
+++ Econom/web/Econom.Web/HtmlHelpers/HtmlHelpers.cs
@@ -0,0 +1,43 @@
+﻿using Econom.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Econom.Web.HtmlHelpers
+{
+    public static class HtmlHelpers
+    {
+        public static MvcHtmlString Image(this HtmlHelper helper,
+                                string url,
+                                string altText,
+                                object htmlAttributes)
+        {
+            TagBuilder builder = new TagBuilder("img");
+            builder.Attributes.Add("src", url);
+            builder.Attributes.Add("alt", altText);
+            builder.MergeAttributes(new RouteValueDictionary(htmlAttributes));
+            return new MvcHtmlString(builder.ToString(TagRenderMode.SelfClosing));
+        }
+
+        public static MvcHtmlString Image(this HtmlHelper helper,
+                                byte[] imageBase64Data,
+                                string altText,
+                                object htmlAttributes)
+        {
+            var source = imageBase64Data == null ?
+              VirtualPathUtility.ToAbsolute(GlobalConstants.DefaultProductImageSource) :
+                string.Format("data:image/png;base64,{0}", Convert.ToBase64String(imageBase64Data));
+
+            TagBuilder builder = new TagBuilder("img");
+            builder.Attributes.Add("src", source);
+            builder.Attributes.Add("alt", altText);
+            builder.MergeAttributes(new RouteValueDictionary(htmlAttributes));
+            return new MvcHtmlString(builder.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
