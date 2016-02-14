11b53be5934e16b8570643411fe9851a8c4f1ad3
MariyaSteffanova <mariya.steffanova@gmail.com>
2016-02-14 10:24:20 +0200

    Product View Model added

---
+++ Econom/web/Econom.Web/Areas/Public/ViewModels/Products/ProductBaseViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Econom.Web.Areas.Public.ViewModels.Products
+{
+    using System.Web.Mvc;
+
+    public class ProductBaseViewModel
+    {
+        public string Barcode { get; set; }
+
+        public string Description { get; set; }
+
+        public byte[] Image { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
