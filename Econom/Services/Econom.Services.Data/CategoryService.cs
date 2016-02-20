87372320d11ccd0556469e90ed13ea5d18f6a7e5
MariyaSteffanova <mariya.steffanova@gmail.com>
2016-02-20 18:40:42 +0200

    [Services] [Categories]

---
+++ Econom/Services/Econom.Services.Data/CategoryService.cs
@@ -0,0 +1,32 @@
+﻿namespace Econom.Services.Data
+{
+    using Econom.Services.Data.Contracts;
+    using System;
+    using System.Linq;
+    using Econom.Data.Models;
+    using Econom.Data;
+    using Econom.Data.Contracts;
+
+    public class CategoryService : ICategoryService
+    {
+
+        private readonly IRepository<IEconomDbContext, Category> categories;
+
+        public CategoryService(IRepository<IEconomDbContext, Category> categories)
+        {
+            this.categories = categories;
+        }
+
+        public Category GetByGS1(string gs1)
+        {
+            return this.categories
+                .All()
+                .FirstOrDefault(x => x.GS1.ToLower().Trim() == gs1.ToLower().Trim());
+        }
+
+        public IQueryable<Category> GetByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Econom/Services/Econom.Services.Data/Contracts/ICategoryService.cs
@@ -0,0 +1,12 @@
+﻿namespace Econom.Services.Data.Contracts
+{
+    using Econom.Data.Models;
+    using System.Linq;
+
+    public interface ICategoryService
+    {
+        IQueryable<Category> GetByName(string name);
+
+        Category GetByGS1(string gs1);
+    }
+}
