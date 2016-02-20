0f3284766ffed1e22fbd17e3a80e1738d7f6737d
MariyaSteffanova <mariya.steffanova@gmail.com>
2016-02-20 17:19:48 +0200

    [Services] [Searchers] Mapper for Product added

---
+++ Econom/Services/Econom.Services.Searchers/Mappers/ProductMap.cs
@@ -0,0 +1,26 @@
+﻿namespace Econom.Services.Searchers.Mappers
+{
+    using Data.Models;
+    using Data.TransferModels;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public static class ProductMap
+    {
+        public static Func<ProductBase, Product> FromBaseProduct = x =>
+        {
+            return new Product
+            {
+                Name = x.Description,
+                Barcode = x.Barcode,
+                //Category 
+                ImageUrl = x.ImageUrl
+            };
+        };
+
+    }
+}
