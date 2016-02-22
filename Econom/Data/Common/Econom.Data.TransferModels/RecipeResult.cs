27ce9d36954f90758321bb50e3dca5fb2e15eab8
MariyaSteffanova <mariya.steffanova@gmail.com>
2016-02-22 02:02:38 +0200

    [Recipes] Transfer models

--- Econom/Data/Common/Econom.Data.TransferModels/RecipeResult.cs
+++ Econom/Data/Common/Econom.Data.TransferModels/RecipeResult.cs
@@ -1,17 +1,23 @@
 ﻿namespace Econom.Services.TransferModels
 {
+    using Newtonsoft.Json;
     using System.Collections.Generic;
 
     public class RecipeResult
     {
-        public int Food2ForkID { get; set; }
+        [JsonProperty("recipe_id")]
+        public string Food2ForkID { get; set; }
 
+        [JsonProperty("title")]
         public string Title { get; set; }
 
+        [JsonProperty("image_url")]
         public string ImageUrl { get; set; }
 
-        public decimal SocialRank { get; set; }
+        [JsonProperty("social_rank")]
+        public string SocialRank { get; set; }
 
+        [JsonProperty("ingredients")]
         public IEnumerable<string> Ingredients { get; set; }
     }
 }
---
+++ Econom/Data/Common/Econom.Data.TransferModels/RecipesQueryResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace Econom.Services.TransferModels
+{
+    using Newtonsoft.Json;
+    using System.Collections.Generic;
+
+    public class RecipesQueryResponse
+    {
+        [JsonProperty("count")]
+        public int Count { get; set; }
+
+        [JsonProperty("recipes")]
+        public ICollection<RecipeResult> Recipes { get; set; }
+    }
+}
