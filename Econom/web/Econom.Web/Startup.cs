1e2d961a70b308bba7db2116eeaf7f7580e382d3
MariyaSteffanova <mariya.steffanova@gmail.com>
2016-02-24 11:28:44 +0200

    SignalR Owin

--- Econom/web/Econom.Web/Startup.cs
+++ Econom/web/Econom.Web/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(Econom.Web.Startup))]
@@ -9,10 +11,19 @@ namespace Econom.Web
     {
         public void Configuration(IAppBuilder app)
         {
-             app.MapSignalR();
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Home/Index")
+            });
+
+            // Use a cookie to temporarily store information about a user logging in with a third party login provider
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            app.MapSignalR();
 
             ConfigureAuth(app);
-          
+
         }
     }
 }
