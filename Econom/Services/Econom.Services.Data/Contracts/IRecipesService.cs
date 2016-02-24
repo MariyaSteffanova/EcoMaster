af4014591e0e07d820061eca8499f1ee5cd527f5
MariyaSteffanova <mariya.steffanova@gmail.com>
2016-02-24 11:52:26 +0200

    [Services][Recipes]

---
+++ Econom/Services/Econom.Services.Data/Contracts/IRecipesService.cs
@@ -0,0 +1,7 @@
+﻿namespace Econom.Services.Data.Contracts
+{
+    public interface IRecipesService
+    {
+        int CreateBasic(string externalId, string title, string imageUrl, decimal rank); // TODO: Ingredients
+    }
+}
---
+++ Econom/Services/Econom.Services.Data/RecipesService.cs
@@ -0,0 +1,39 @@
+﻿using Econom.Data;
+using Econom.Data.Contracts;
+using Econom.Data.Models;
+using Econom.Services.Data.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Econom.Services.Data
+{
+    public class RecipesService : IRecipesService
+    {
+        private readonly IRepository<IEconomDbContext, Recipe> recipes;
+
+        public RecipesService(IRepository<IEconomDbContext, Recipe> recipes)
+        {
+            this.recipes = recipes;
+        }
+
+        public int CreateBasic(string externalId, string title, string imageUrl, decimal rank)
+        {
+            var recipe = new Recipe
+            {
+                Title = title,
+                ExternalID = externalId,
+                ImageUrl = imageUrl,
+                SocialRank = rank
+            };
+
+            this.recipes.Add(recipes);
+            this.recipes.SaveChanges();
+
+            return recipe.ID;
+
+        }
+    }
+}
