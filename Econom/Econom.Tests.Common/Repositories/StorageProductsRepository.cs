f8bec1c64432dba5c7f78e54904aa378585e8446
MariyaSteffanova <mariya.steffanova@gmail.com>
2016-02-24 17:26:47 +0200

    Services init.StorageRepoMock

---
+++ Econom/Econom.Tests.Common/Repositories/StorageProductsRepository.cs
@@ -0,0 +1,32 @@
+﻿namespace Econom.Tests.Common.Repositories
+{
+    using Econom.Data.Models;
+    using System;
+    using System.Collections.Generic;
+
+    public class StorageProductsRepository
+    {
+        private static List<string> foods = new List<string>
+        { "apple", "orange", "banana", "boiled beans", "smashed potatoes", "baked", "healthy", "strong" ,
+        "deliciuous", "all baked", "organic apple", "nature food"};
+
+        public static List<StorageProduct> Get()
+        {
+            var products = new List<StorageProduct>();
+
+            for (int i = 0; i < foods.Count; i++)
+            {
+                products.Add(new StorageProduct
+                {
+                    ID = i,
+                    CreatedOn = DateTime.Now.AddDays(i * (-1)),
+                    Product = new Product
+                    {
+                        Name = foods[i]
+                    }
+                });
+            }
+            return products;
+        }
+    }
+}
